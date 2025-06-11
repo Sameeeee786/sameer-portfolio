75945734d711b1250028b8306ff2e979fc64714d
sameer ahmad ansari <sameerahmadansari@MacBookAir.lan>
2025-06-12 00:53:20 +0530

    Added working Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center py-6 mt-10">
+      <p>© 2025 Sameer Ahmad. All rights reserved.</p>
+    </footer>
+  );
+}
